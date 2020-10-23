08a5decacc947e9f571eb884397c85a7c9761420
Leemcmaw <62882612+Leemcmaw@users.noreply.github.com>
2020-10-23 16:24:59 +0100

    Add files via upload

---
+++ SimpleBankApp/src/main/java/Coursework/Account.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Coursework;
+
+/**
+ *
+ * @author leest
+ */
+public class Account {
+    private String accountNumber;
+    private String accountHolder;
+    private String openDate;
+    private double balance;
+    
+    public Account(String accNumber, String accHolder, String date, double accBalance){
+        this.accountNumber = accNumber;
+        this.accountHolder = accHolder;
+        this.openDate = date;
+        this.balance = accBalance;
+    }
+    
+    public void withdraw(double amount){
+                  
+        this.balance = this.balance - amount;
+    }
+    
+    public void deposit(double amount){
+        this.balance = this.balance + amount;
+    }
+    
+    public void transfer(Account a, double amount){
+        this.withdraw(amount);
+        a.deposit(amount);
+        
+    }
+    
+    public String getaccountHolder(){
+        return this.accountHolder;
+    }
+    
+    public String getaccountNumber(){
+        return this.accountNumber;
+    }
+    
+    public String getOpenDate(){
+        return this.openDate;
+        
+    }
+    
+    
+    public double getBalance(){
+        return this.balance;
+    }
+    
+    @Override
+    public String toString(){
+        return this.accountNumber;
+    }
+}
